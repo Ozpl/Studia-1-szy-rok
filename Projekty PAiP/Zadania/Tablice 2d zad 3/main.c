47d4921b0b60c469b5eeeb89bad4912766eb9331
Ozpl <ozpl18@gmail.com>
2017-12-11 18:06:48 +0100

    11.12

--- Projekty PAiP/Zadania/Tablice 2d zad 3/main.c
+++ Projekty PAiP/Zadania/Tablice 2d zad 3/main.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 int main() {
     int x[10][10], y[10], z[10];
-    int i = 0, j = 0, m = 0, n = 0, nieuje = 0, najkol = 0, inajkol = 0, a = 0, b = 80;
+    int i = 0, j = 0, m = 0, n = 0, nieuje = 0, najkol = 0, inajkol = 0;
 
-    printf("Podaj rozmiar m macierzy kwadratowej = ");
+    printf("Podaj rozmiar m tablicy = ");
     scanf("%i",&m);
-    printf("Podaj rozmiar n macierzy kwadratowej = ");
+
+    printf("\nPodaj rozmiar n tablicy = ");
     scanf("%i",&n);
-    srand(time(NULL));
 
-    printf("\nLosowanie liczby z przedzialu <a,b> = <%i,%i>", a-30, b-30);
-
+    printf("\n\nPodaj elementy tej tablicy = ");
     for (i = 0; i < m; i++) {
         for (j = 0; j < n; j++) {
-            x[i][j]=rand()%(b-a+1)-30;
+            printf("\nx[%i][%i] = ",i,j);
+            scanf("%i",&x[i][j]);
         }
     }
 
-    printf("\n\nPodana tablica = \n");
+    printf("\n\nWpisana tablica = \n");
     for (i = 0; i < m; i++) {
         for (j = 0; j < n; j++) {
             printf("%5i",x[i][j]);
         }
         printf("\n");
     }
+
     for (j = 0; j < n; j ++) {
         for (i = 0; i < m; i++) {
             if (x[i][j] < 0 && x[i][j] % 2 != 0) {
