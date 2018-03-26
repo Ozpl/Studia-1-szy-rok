45291dc763f964a0e5bbe93bd7c5aa67c090e641
Ozpl <ozpl18@gmail.com>
2018-03-26 21:23:46 +0200

    26.03

--- Matlab/Semestr 2/Metody matematyczne w automatyce/Sprawko2.m
+++ Matlab/Semestr 2/Metody matematyczne w automatyce/Sprawko2.m
@@ -18,12 +18,50 @@ for i=1:1:length(t)
         u2(i) = 0;
     end
 end
-% figure;
-% plot(t,u-u2);
-% xlim([-5 5]);
-% ylim([-1 2]);
+figure;
+plot(t,u-u2);
+xlim([-1 4]);
+ylim([-1 2]);
 
-syms t omega;
-f = 1*exp(-1i*omega*t);
-F = int(f,t,0,pi);
-z = abs(sqrt((sin(omega*t)/omega)^2 + (2*sin((omega*2)/2)/omega)^2));
+clear;
+close all;
+%a jako omega we wzorach
+a = linspace(-5,5,1000);
+%widmo Grzesiu sygnal 1
+b = sqrt((-sin(a.*pi)./a).^2+(2*(sin(-pi.*a*0.5).^2)).^2);
+figure;
+plot(a,b);
+
+%faza Grzesiu sygnal 1
+c = atan((2*(sin(-pi.*a*0.5).^2))./(-sin(a.*pi)./a));
+figure;
+plot(a,c);
+
+syms g;
+d = vpaintegral(((sin(g*pi)/g)-(sqrt(-1)*2*(sin(g*pi*0.5))^2))^2,g,0,1);
+
+%widmo Grzesiu sygnal 2
+f = abs((-1+(cos(-pi.*a)+1i*sin(-pi.*a)).*(1+a.*sqrt(-1)*pi))./(a.^2*pi));
+figure;
+plot(a,f);
+
+%faza Grzesiu sygnal 2
+h = angle((-1+(cos(-pi.*a)+1i*sin(-pi.*a)).*(1+a.*sqrt(-1)*pi))./(a.^2*pi));
+figure;
+plot(a,h);
+
+syms f;
+j = vpaintegral(((-1+(cos(-pi*f)+sqrt(-1)*sin(-pi*f))*(1+f*sqrt(-1)*pi))/(f^2))^2,f,0,1);
+
+%widmo Piotr sygnal 1
+amp1 = abs(sqrt( ( (4.*sin( (pi.*a)/2) ).^4 / a.^2 ) + ( (sin(pi.*a) ).^2 ) ./ (a.^2) ));
+figure;
+plot(a,amp1);
+
+%faza Piotr sygnal 1
+faz1 = ( atan( (-2.*sin( (a.*pi) / (2) ).^2 ) ./ (sin(a.*pi)) ) );
+figure;
+plot(a,faz1);
+
+syms omega;
+pars1 = (1/pi).* vpaintegral(( ( (sin(omega*pi)) / (omega) ) - 1i.*( (2.*sin((omega.*pi) / (2)).^2) / (omega) ) ).^2,omega,0,1);
